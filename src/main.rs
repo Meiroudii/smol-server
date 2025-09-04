d94dc7f498b2e416e979de0bbdafe9e3bd4d6ade
Meiroudii <119019700+Meiroudii@users.noreply.github.com>
2025-09-05 01:26:43 +0900

    feat: add reading to the requests

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
-use std::net::TcpListener;
+use std::{
+    io::{BufReader, prelude::*},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
-
     let listener = TcpListener::bind("127.0.0.1:7879").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
         println!("{:#?}", stream);
-        println!("Connection established.");
+        invoke_link(stream);
     }
 
+}
+
+fn invoke_link(mut stream: TcpStream) {
+    let buffr_reader = BufReader::new(&stream);
+    let http_request: Vec<_> = buffr_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("\tRequest: {http_request:#?}");
 }
